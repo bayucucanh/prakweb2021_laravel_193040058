11b07109d5ef4eda95ac6cc4aa694d440116c3d3
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2021-10-21 10:20:01 +0700

    Menyelesaikan Latihan mengenai N+1 Problem dan commit ulang latihan video 10 yang ke reset

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,9 +46,15 @@ Route::get('/categories', function () {
 
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post By Category : $category->name",
+        'posts' => $category->posts->load('category', 'author')
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
