f258c096340c535a85320a691b13674fdf814e9b
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2021-11-18 17:13:08 +0700

    Menyelesaikan Latihan mengenai Validation & Insert Post pada video ke 19

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -15,7 +15,7 @@
       <article class="my-3 fs-5">
         {!! $post->body !!}
       </article>
-      <a href="/posts" class="d-block mt-3 text-decoration-none" >Back to Posts</a>
+      
     </div>
   </div>
 </div>
