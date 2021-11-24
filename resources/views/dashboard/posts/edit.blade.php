038a1946f73682a3eba9d0627d696d26703ea641
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2021-11-24 15:26:37 +0700

    Menyelesaikan Latihan mengenai Preview, Update & Delete Image pada video ke 22

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -6,9 +6,11 @@
 </div>
 
 <div class="col-lg-8">
-  <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+  <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
     @method('put')
     @csrf
+
+    {{-- Title --}}
     <div class="mb-3">
       <label for="title" class="form-label">Title</label>
       <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title', $post->title) }}">
@@ -18,6 +20,8 @@
           </div>
       @enderror
     </div>
+
+    {{-- Slug --}}
     <div class="mb-3">
       <label for="slug" class="form-label">Slug</label>
       <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug', $post->slug) }}">
@@ -27,6 +31,8 @@
           </div>
       @enderror
     </div>
+
+    {{-- Category --}}
     <div class="mb-3">
       <label for="category" class="form-label">Category</label>
       <select class="form-select" name="category_id">
@@ -40,6 +46,24 @@
       </select>
     </div>
 
+    {{-- File Input --}}
+    <div class="mb-3">
+      <label for="image" class="form-label">Post Image</label>
+      <input type="hidden" name="oldImage" value="{{ $post->image }}">
+      @if($post->image)
+        <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+      @else
+        <img class="img-preview img-fluid mb-3 col-sm-5">
+      @endif
+      <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+      @error('image')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+      @enderror
+    </div>
+
+    {{-- Body --}}
     <div class="mb-3">
       <label for="body" class="form-label">Body</label>
       @error('body')
@@ -65,5 +89,20 @@
   document.addEventListener('trix-file-accept', function(e) {
     e.preventDefault();
   })
+
+  function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+
+    oFReader.onload = function(oFREvent) {
+      imgPreview.src = oFREvent.target.result;
+    }
+  }
 </script>
 @endsection
